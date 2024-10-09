edd5407eed1e826ed7070e029c8b274a77ff6ee5
gvinod0007 <gvinod0007@gmail.com>
2024-10-09 06:09:21 +0530

    jenkinsfile and pipeline will be automatically run because of webhook.

--- terraform_code/ec2.tf
+++ terraform_code/ec2.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efdafd5b4e4202cd67983eef22ef3f5d395aaf4fb5c7d6d275626e178464757c
-size 2512
+oid sha256:4109685f8d28ef7115aa7bdba4a82b88d9a72090dc9f8d8730b245f909c30aca
+size 2517
